02d9ab3f974870cc684de3627bdc0e2be18d5c45
markgmz92 <markgmz92@gmail.com>
2022-05-12 19:19:19 +0800

    [MU - 036] Added cart functionality

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link, useLocation, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card
+} from 'react-bootstrap';
+import Message from '../components/Message';
+import { addToCart } from '../actions/cartActions';
 
 const CartScreen = () => {
+  const { id } = useParams();
+  const qty = useLocation();
+  console.log(qty);
+  const dispatch = useDispatch();
+  console.log(dispatch);
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  useEffect(() => {
+    if (id) {
+      dispatch(addToCart(id, qty));
+    }
+  }, [dispatch, id, qty]);
   return <div>CartScreen</div>;
 };
 
--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -19,7 +19,7 @@ import { listProductDetails } from '../actions/productActions';
 const ProductScreen = () => {
   const navigate = useNavigate();
   const { id } = useParams();
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
   const dispatch = useDispatch();
 
   const productDetails = useSelector((state) => state.productDetails);
