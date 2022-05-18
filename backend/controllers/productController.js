f116aca5ed8a55600e72aeb587b46f79da8f250e
markgmz92 <markgmz92@gmail.com>
2022-05-18 20:09:28 +0800

    [MU - 039] Created user authentication endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,10 +1,10 @@
-import AsyncHandler from 'express-async-handler';
+import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
 
 // @desc Fetch all products
 // @route GET /api/products
 // @access Public
-const getProducts = AsyncHandler(async (req, res) => {
+const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
 
   res.json(products);
@@ -13,7 +13,7 @@ const getProducts = AsyncHandler(async (req, res) => {
 // @desc Fetch single product
 // @route GET /api/products/:id
 // @access Public
-const getProductById = AsyncHandler(async (req, res) => {
+const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
   if (product) {
     res.json(product);
