85de531d42e8701422abacf8af79a0db7818ea89
markgmz92 <markgmz92@gmail.com>
2022-05-25 02:30:23 +0800

    [MU - 053] Removed onClick from submit button

--- client/src/screens/LoginScreen.js
+++ client/src/screens/LoginScreen.js
@@ -53,7 +53,7 @@ const LoginScreen = () => {
           ></Form.Control>
         </Form.Group>
 
-        <Button type='submit' variant='primary' onClick={() => navigate('/')}>
+        <Button type='submit' variant='primary'>
           {' '}
           Sign In
         </Button>
