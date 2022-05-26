3a7b7042c9db500f13aeba4e74cc71a5d1f3e593
markgmz92 <markgmz92@gmail.com>
2022-05-26 18:46:42 +0800

    [MU - 059] Created order routes on the backend

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 const port = process.env.PORT || 5000;
 
 dotenv.config();
@@ -20,6 +21,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/order', orderRoutes);
 
 app.use(notFound);
 
