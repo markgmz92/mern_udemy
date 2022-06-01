789b5786500ef8b6fe224e675a6a22650e5b333f
markgmz92 <markgmz92@gmail.com>
2022-06-01 17:35:12 +0800

    [MU - 070] Added orders on profile

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -82,7 +82,8 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 // @route GET /api/orders/myorders
 // @access Private
 const getMyOrders = asyncHandler(async (req, res) => {
-  const orders = await Order.findById({ user: req.user_id });
+  const orders = await Order.find({ user: req.user._id });
+  res.json(orders);
 });
 
 export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
